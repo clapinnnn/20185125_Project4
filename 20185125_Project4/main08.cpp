fa3e524d0f7efefaab449a61f9d5ead988745c7c
1506 <1506@DESKTOP-FM6Q1M7>
2022-11-27 16:28:01 +0900

    수정

---
+++ 20185125_Project4/main08.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+T getSmallest(T arr[], int n) {
+	T min = arr[0];
+	for (int i = 1; i < n; i++)
+		if (arr[i] < min)
+			min = arr[i];
+	return min;
+}
+
+int main() {
+	double list[]{ 1.2, 3.3, 9.0, 1.5, 8.7 };
+	cout << "ÃÖ¼Ò°ª: " << getSmallest(list, 5) << endl;
+}
